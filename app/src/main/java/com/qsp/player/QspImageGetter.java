37d27e76a8e706c833474ae78b9c4261b6a7ae76
Kirill Ashikhmin <boom.vrn@gmail.com>
2016-08-31 00:09:06 +0300

    Search-for-images-case-insensitively

--- app/src/main/java/com/qsp/player/QspImageGetter.java
+++ app/src/main/java/com/qsp/player/QspImageGetter.java
@@ -2,6 +2,10 @@ package com.qsp.player;
 
 import android.graphics.drawable.Drawable;
 import android.text.Html.ImageGetter;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
 
 public class QspImageGetter implements ImageGetter {
 	private String mDirectory;
@@ -22,22 +26,48 @@ public class QspImageGetter implements ImageGetter {
           Drawable drawable = null;
           if ((source != null) && (!source.contains("://")))
           {
-        	  source = mDirectory.concat(source);
-              drawable = Drawable.createFromPath(source);
-              if (drawable != null)
-              {
-	              int nWidth = (int) ((int)drawable.getIntrinsicWidth()*0.75);
-	              int nHeight = (int) ((int)drawable.getIntrinsicHeight()*0.75);
-	              
-	              if (nWidth>mScreenWidth)
-	              {
-	            	  float k = mScreenWidth;
-	            	  k = k/nWidth;
-	            	  nWidth = mScreenWidth;
-	            	  nHeight = (int) ((int)nHeight*k);
-	              }
-	              
-	              drawable.setBounds(0, 0, nWidth, nHeight);
+              File path = new File(mDirectory.concat(source)); // Fallback
+
+              // Even though File.exists may be case insensitive, for some weird reason
+              // the Drawable.createFromPath may be case sensitive and will fail.
+              // So, we're searching for the resource here no matter what path.exists() may return.
+              File curPath = new File(mDirectory);
+              String[] segments = source.split("[/\\\\]");
+              boolean found = false;
+              for (int i = 0; i < segments.length; i++) {
+                  String segment = segments[i];
+                  boolean lastSegment = i == segments.length - 1;
+                  found = false;
+                  for (File f : curPath.listFiles()) {
+                      if ((lastSegment ? f.isFile() : f.isDirectory())
+                              && f.getName().equalsIgnoreCase(segment)) {
+                          found = true;
+                          curPath = f;
+                          break;
+                      }
+                  }
+              }
+              if (found && curPath.exists()) { path = curPath; }
+
+              if (path.exists()) {
+                  try {
+                      drawable = Drawable.createFromPath(path.getCanonicalPath());
+                  } catch (IOException e) {
+                      e.printStackTrace();
+                  }
+                  if (drawable != null) {
+                      int nWidth = (int) ((int) drawable.getIntrinsicWidth() * 0.75);
+                      int nHeight = (int) ((int) drawable.getIntrinsicHeight() * 0.75);
+
+                      if (nWidth > mScreenWidth) {
+                          float k = mScreenWidth;
+                          k = k / nWidth;
+                          nWidth = mScreenWidth;
+                          nHeight = (int) ((int) nHeight * k);
+                      }
+
+                      drawable.setBounds(0, 0, nWidth, nHeight);
+                  }
               }
           }
           return drawable;
